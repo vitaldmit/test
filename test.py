51aefa95d9cc00a90063dadd04e2cd5137a5eb2b
Виталий Дмитриев <vitaldmit@bk.ru>
2025-01-12 17:14:42 +0300

    Remove all except `name`, `age`, `gender` (#5)
    * Remove all except `name`, `age`, `gender`
    
    * Remove `gender`

--- test.py
+++ test.py
@@ -3,26 +3,12 @@ class Test:
     def __init__(self):
         self.name = "Test"
         self.age = 20
-        self.gender = "Male"
-        self.height = 170
-        self.weight = 60
-        self.bmi = 20
-        self.bmi_category = "Normal"
 
     def get_name(self):
         return self.name
 
     def get_age(self):
         return self.age
-
-    def get_gender(self):
-        return self.gender
-
-    def get_height(self):
-        return self.height
-
-    def get_weight(self):
-        return self.weight
 
 
 test = Test()
