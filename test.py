88c4ab3796e4f59508b81f6d1b93306d1c91ca39
Виталий Дмитриев <vitaldmit@bk.ru>
2025-01-12 17:13:20 +0300

    Remove `gender`

--- test.py
+++ test.py
@@ -3,17 +3,12 @@ class Test:
     def __init__(self):
         self.name = "Test"
         self.age = 20
-        self.gender = "Male"
-
 
     def get_name(self):
         return self.name
 
     def get_age(self):
         return self.age
-
-    def get_gender(self):
-        return self.gender
 
 
 test = Test()
