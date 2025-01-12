33f0476030d81e4d4ae74e3f1182b4d87b1c5d61
Виталий Дмитриев <vitaldmit@bk.ru>
2025-01-12 20:21:38 +0300

    Remove another

--- test.py
+++ test.py
@@ -3,12 +3,5 @@ class Test:
     def __init__(self):
         self.name = "Test"
 
-    def get_name(self):
-        return self.name
-
-    def set_name(self, name):
-        self.name = name
-
 
 test = Test()
-print(test.get_name())
