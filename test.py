fe14c7ff2844d5c3adc102a35b4cc86e673d9fa0
Виталий Дмитриев <vitaldmit@bk.ru>
2025-01-12 17:50:50 +0300

    Add `gender` (#11)

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ class Test:
     def __init__(self):
         self.name = "Test"
         self.age = 20
+        self.gender = "Male"
 
     def get_name(self):
         return self.name
@@ -10,11 +11,17 @@ class Test:
     def get_age(self):
         return self.age
 
+    def get_gender(self):
+        return self.gender
+
     def set_name(self, name):
         self.name = name
 
     def set_age(self, age):
         self.age = age
+
+    def set_gender(self, gender):
+        self.gender = gender
 
 
 test = Test()
